0c9b0e805a9c73deb7e6538cff00f7a809b002d5
蔡洋 <saywhy@caiyangdeMacBook-Pro.local>
2019-04-15 16:07:53 +0800

    first commit

--- README.md
+++ README.md
@@ -1,72 +1,19 @@
-# 2233
-- 个人github上，太多烂尾的库，综合一波。。
-- 这个库里将放各种bili上的code就是了。。
-- 2019/02/08 再次维护这堆项目（能跑就行），维护完了
-- 之后如果有心情，加上动态的抢楼吧。然后抢楼的代码写得太丑了都看不下去了，还好封装的还行。不需要看太多就改好了，有兴趣再改一下那些headers的代码
+## 弹幕爬取
 
-## 目录结构
+### 使用方法：
 
-- [danmuku.py](#弹幕爬虫说明) --- 弹幕爬虫
-- [robfloor.py](#BILI_RobFloor) --- 抢楼器
-- [dosign.py](#直播签到与银瓜子换硬币) --- 直播签到，银瓜子换硬币脚本
-- old --- 存放旧代码（git会有记录，还是有点麻烦
+先获取所要爬取弹幕电影的cid号，调用脚本时输入cid号，输出的文件名即可，脚本会自动在当前目录下生成一个csv文件,所爬取的信息有发送内容，影片时间，发送时间，发送用户的ID
 
-
-## BILI_RobFloor
-
-### 说明
-
-- 自制B站抢楼器
-- 账号被封一概不负责
-- 目前基本就是封装调用了三个api。。。orz
-
-### 使用方法
-
-- 当前目录下bilicookies文件存里登录cookie信息
-- 修改task函数，bi.run()传入相应的参数
-- 线程数，线程的等待自行调整
-
-BILI类内，run(av_num, floor, content)，av_num为要抢的视频号，floor是要抢的楼层，content是要发的评论的内容。不传入av_num则是搜索订阅的最新视频。
-
-（多线程没有锁，所以有可能重复发...注意）
-
-动态评论与视频评论现在会检测一个字段csrf，在cookies里有，加上这个就行了
-
-## 直播签到与银瓜子换硬币
-
-- 当前目录下bilicookies文件存里登录cookie信息（最好是保存成功签到时用的cookies）
-- 然后执行dosign.py。即可
-- 若想自动化执行，找个服务器，Linux下可用 crontab设置定时任务。
-- silver2coin函数会使用银瓜子兑换硬币（升级用），自行决定使用与否，默认注释
-
-
-
-
-## 弹幕爬虫说明
-
-- 目前是爬取指定av号或ep号番剧的弹幕存到本地
-- 弹幕信息有内容，时间，和时间2233
-
-### 使用方法
-
-```powershell
-PS E:\GITWORKS\BILI> python .\danmuku.py -h
-usage: danmuku.py [-h] [-i INPUT] [-o OUTPUT]
-
-Welcome to BILI
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -i INPUT, --input INPUT
-                        set the av_number or eq_number to crawl
-  -o OUTPUT, --output OUTPUT
-                        set the filename to store
-PS E:\GITWORKS\BILI> python .\danmuku.py -i av42807128 -o danmu.csv
-42807128
-cid = 75051785
-写入弹幕ing...
-写入完成...请查看danmu.csv
+```bash
+python bili_danmu_spider.py -i 'cid' -o 'filename'
 ```
+### 参数说明：
+1. ``-i``：输入excel文件的名称
+2. ``-o``：输入数据要处理的列明
+3. ``-n``或``--ncores``：处理是并行的线程数
+示例：python bili_danmu_spider.py -i xxxxxxxx -o test.csv
 
-
-
+如需要查看命令行参数可输入
+```bash 
+python bili_danmu_spider.py -h
+```
--- bili_danmu_spider.py
+++ bili_danmu_spider.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 09 02:27:43 2016
-@author: Tmn07
-@update: 2019/02/08 20点57分
-"""
 
 from bs4 import BeautifulSoup
 import requests
@@ -32,49 +27,28 @@ def get_danmuku(cid, filename):
         for danmu in danmus:
             content = danmu.string
             attr = danmu['p'].split(',')
-            t1 = str(attr[0])  # 视频中的时间
+            t1 = str(attr[0])  # 电影时间
             t2 = attr[4]  # 发布时间
+            t3 = str(attr[6])  # 发送者ID
             timestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(t2)))
-            fw.write(content.encode('utf-8')+ ',' + t1 + ',' + timestr + '\n')
+            fw.write(content.encode('utf-8')+ ',' + t1 + ',' + timestr + ',' + t3 + '\n')
     print(u"写入完成...请查看"+filename)   
 
-# mode 1 是输入av12345678
-# mode 2 是输入ep1234567 （番剧）
-def get_cid(aid, mode=1):
-    # print(aid)
-    if mode == 1:
-        url = "https://www.bilibili.com/video/av"+aid
-        r = requests.get(url, headers=header)
-        match_list = re.findall('"aid":"'+aid+'".*?"cid":\d*?,', r.text)
-    elif mode == 2:
-        url = "https://www.bilibili.com/bangumi/play/ep"
-        r = requests.get(url+aid, headers=header)
-        match_list = re.findall(',"id":'+aid+'.*?"cid":\d*?,', r.text)
-        # write_down(r.content)
-    # print (match_list)
-    cid = match_list[0][match_list[0].index('cid')+5:-1]
-    print ("cid = " + cid)
-    return cid
-
 def get_input_id():
-    parser = argparse.ArgumentParser(description='Welcome to BILI')
-    parser.add_argument('-i', '--input', help='set the av_number or eq_number to crawl')
+    parser = argparse.ArgumentParser(description='弹幕爬取脚本说明')
+    parser.add_argument('-i', '--input', help='set the cid')
     parser.add_argument('-o', '--output',  help='set the filename to store')
     args = parser.parse_args()
     if args.output:
         filename = args.output
     else:
         filename = 'test.csv'
-    aid = str(args.input)
-    if aid.startswith('av'):
-        return (aid[2:], 1, filename)
-    if aid.startswith('ep'):
-        return (aid[2:], 2, filename)
-    return (aid, 1, filename)
+    cid = str(args.input)
+    print ("cid = " + cid)
+    return (cid, filename)
 
 def main():
-    aid, mode, filename = get_input_id()
-    cid = get_cid(aid, mode)
+    cid, filename = get_input_id()
     get_danmuku(cid, filename)
     
 if __name__ == '__main__':
